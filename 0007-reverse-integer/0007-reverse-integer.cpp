5a7a475ccc69042148593faf19c399a24227bede
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-02-08 15:02:54 +0530

    Time: 2 ms (38.36%), Space: 8.6 MB (53.79%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,10 +5,11 @@ public:
         while (x!=0){
             int digit = x%10;
 
-            if( (answer < INT_MIN/10) || (answer > INT_MAX/10) ){
+            if( (answer > INT_MAX/10) || (answer < INT_MIN/10) ){
                 return 0;
             }
             answer = answer *10 + digit;
+            x = x/10;
         }
         return answer;
     }
