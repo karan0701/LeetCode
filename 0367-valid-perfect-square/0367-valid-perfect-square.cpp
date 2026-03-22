67e2398d281e140845ecafbfff605f629ebfb7ce
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-03-22 17:56:03 +0530

    Time: 0 ms (100%), Space: 7.7 MB (53.21%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for(long i=1; i*i<= num; i++){
+        for(long i=1; i*i<= num; i++){   // ( we use long instead of long because at the square of 46341 the valsue of int. is overflowed)
             if(i*i ==num)
             return true;
         }
