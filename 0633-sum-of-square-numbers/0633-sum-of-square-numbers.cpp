813d8b5e61512a578538bd9ffbaa60ed21c053a1
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-03-22 18:51:20 +0530

    Time: 3 ms (32.03%), Space: 7.7 MB (55.24%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        class Solution {
-public:
-    bool judgeSquareSum(int c) {
         for (long a = 0; a * a <= c; a++) {  // Iterate through all possible values of `a`
             double b = sqrt(c - a * a);      // Compute `b` as the square root of `c - a^2`
             if (b == (int)b) {                // Check if `b` is an integer
@@ -11,8 +8,5 @@ public:
             }
         }
         return false;                         // If no such pair `(a, b)` is found, return false
-    }
-};
-       
     }
 };
