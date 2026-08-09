20591bede7d1d720a68ca91b0fa74b82e25a0b30
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-08-10 01:01:52 +0530

    Time: 0 ms (100%), Space: 13.7 MB (44.08%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int x) {
+    int searchInsert(vector<int>& nums, int target) {
 
         //same approach hai jaise lower bound ko find krte hai vaise hii
 
         int n = nums.size();
         int low = 0;
         int high = n-1;
-        int ans = n;
         while(low<=high){
             int mid = low + (high-low)/2;
-            if(nums[mid]>=x){
-                ans = mid;
+            if(nums[mid]==target){
+               return mid;
+            }
+            else if(nums[mid]>target){
                 high = mid-1;   // looking for more small index on left
             }
             else{
                 low = mid +1;  // looking for right
             }
         }
-        return ans ;
+        return low ;
         
     }
 };
