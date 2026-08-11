67812a51a8b3340da3de9737e1517a40fb8e89cb
Karan Pratap Singh <priyanshujadoun12345@gmail.com>
2026-08-11 19:11:09 +0530

    Time: 0 ms (100%), Space: 26.3 MB (36.15%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        int s = 0;
-        int e= n-1;
-        while(s<e){
-            int mid = s +(e-s)/2;
-            if((mid%2 ==0 && mid+1<n && nums[mid]== nums[mid+1]) ||
-             mid%2 ==1 && mid-1 >=0 && nums[mid] ==nums[mid-1])   // mid+1 and -1><n .... corner cases hai... jisse ki out of index na aa jaye and
-            s = mid+1;
-            else
-            e = mid;
-        if(mid+1<n && mid-1 >=0 && nums[mid]!= nums[mid+1] && nums[mid] !=nums[mid-1] ) return nums[mid];
-        }
-        
-    return nums[s];
-    }
 
+        int s = 0;
+        int e = nums.size() - 1;
+
+        while(s < e) {
+
+            int mid = s + (e - s) / 2;
+
+            // Make mid even
+            if(mid % 2 == 1)
+                mid--;
+
+            // Correct pair
+            if(nums[mid] == nums[mid + 1]) {
+                s = mid + 2;
+            }
+            else {
+                e = mid;
+            }
+        }
+
+        return nums[s];
+    }
 };
